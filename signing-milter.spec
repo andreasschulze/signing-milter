cec96976a2ab9cd2467c0abf386e1ec7a581f777
A. Schulze <git@andeasschulze.de>
2020-07-19 13:30:19 +0200

    release 2019012801 checked in

---
+++ signing-milter.spec
@@ -0,0 +1,87 @@
+#
+#  signing-milter - signing-milter.spec
+#  Copyright (C) 2010-2019  Andreas Schulze
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; only version 2 of the License is applicable.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#  Authors:
+#    Andreas Schulze <signing-milter at andreasschulze.de>
+#
+#
+
+Name:		signing-milter
+Version:	20190125
+%define		debian_version %{version}01
+Release:	1.1
+Summary:	Sign email via milter protocol
+License:	GPL-2.0
+Group:		Productivity/Networking/Email/Utilities
+Url:		https://signing-milter.org
+Source:		https://signing-milter.org/signing-milter_%{debian_version}.tar.xz
+BuildRoot:	%{_tmppath}/%{name}
+BuildRequires:	pkg-config, xz, pwdutils
+BuildRequires:	sendmail-devel >= 8.14, tinycdb-devel >= 0.77, openssl-devel
+Requires:	cron
+
+%description
+signing-milter  uses  the  milter  interface, originally distributed as
+part of version 8.11 of sendmail(8), to  provide  signing  service  for
+mail transiting a milter-aware MTA.
+
+%prep
+%setup -n %{name}
+
+%build
+make
+
+%install
+make DESTDIR=%{buildroot} install
+
+%pre
+getent group signing-milter 2>/dev/null || {
+  groupadd -o -g 30053 signing-milter || :
+}
+getent passwd signing-milter 2>/dev/null || {
+  useradd -o -u 30053 -g signing-milter -c "signing-milter" -d /etc/signing-milter -s /bin/false signing-milter || :
+  chage --lastday -1 --mindays -1 --maxdays -1 --warndays -1 --inactive -1 --expiredate -1 signing-milter || :
+}
+
+%post
+if [ ! -L /etc/service/signing-milter ]; then
+    echo " - richte signing-milter ein ( starte aber noch nicht )"
+    touch /var/lib/supervise/signing-milter/down
+    ln -s ../../var/lib/supervise/signing-milter /etc/service/ || :
+else
+    echo "- starte signing-milter neu"
+    /usr/bin/svc -t /var/lib/supervise/signing-milter || :
+fi
+
+%files
+%defattr(-,root,root)
+%doc INSTALL README debian/changelog debian/copyright
+%config %{_sysconfdir}/cron.daily/signing-milter
+%config %{_sysconfdir}/cron.hourly/signing-milter
+%config(noreplace) %{_sysconfdir}/default/signing-milter
+%attr(0750,root,signing-milter) %dir %{_sysconfdir}/signing-milter/
+%{_sbindir}/signing-milter
+%{_mandir}/man8/signing-milter.8*
+%dir %{_prefix}/share/signing-milter/
+%{_prefix}/share/signing-milter/run_signing-milter
+%dir /var/lib/supervise/signing-milter/
+/var/lib/supervise/signing-milter/run
+
+%changelog
+* Sun Aug 22 2010 <signing-milter@andreasschulze.de> - 20100822
+- komplettes Changelog nur unter
+  /usr/share/doc/packages/signing-milter/changelog
---
+++ utils/break_after_semicolon.c
@@ -0,0 +1,92 @@
+/*
+ * signing-milter - utils/break_after_semicolon.c
+ * Copyright (C) 2010-2018  Andreas Schulze
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; only version 2 of the License is applicable.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Authors:
+ *   Andreas Schulze <signing-milter at andreasschulze.de>
+ *
+ */
+
+#include "break_after_semicolon.h"
+
+/*
+ * Ersetzt in einem String das ";" gefolgt von einem beliebigen Zeichen
+ * in der Phase vor dem Signieren durch "; \r \n \t"
+ * und in der Phase nach dem Signieren durch "; \n \t"
+ * Annahme : nach einem ; kommt immer ein Leerzeichen. Dies ist jedoch durch einen Aufruf von hdrdup sichergestellt.
+ *
+ * Argument: - ein mit malloc allokierter Speicherbereich mit einem nullterminierten String
+ *           - PHASE_PRE_SIGN (3) oder PHASE_POST_SIGN (2)
+ * Rückgabe: - im Fehlerfall:
+ *             NULL
+ *           - wenn string keine ; enthielt:
+ *             der urspüngliche String
+ *           - wenn der String < 70 Zeichen ist:
+ *             der urspüngliche String
+ *           - wenn string mindestens ein ; enthielt:
+ *             ein neuer, mit malloc allokierter Speicher.
+ *             der als Argument übergebene Speicher ist mit free bereinigt.
+ */
+char* break_after_semicolon(char* string, int phase) {
+
+    int    num_semicolon = -1;
+    char*  new_string;
+    char*  p_old;
+    char*  p_new;
+
+    if ((num_semicolon = get_num_semicolons(string)) < 0) {
+        return(NULL);
+    }
+
+    if (!num_semicolon) {
+        /* keine semikolons in string enthalten */
+        return (string);
+    }
+
+    /* pro Semokolon 2 oder 3 zusätzliches Byte + ein Leerzeichen (oder Luft?) */
+    if ((new_string = malloc(strlen(string) + (num_semicolon*phase) + 1)) == NULL) {
+        logmsg(LOG_ERR, "FATAL: break_after_semicolon: malloc failed");
+        return(NULL);
+    }
+
+    p_old = string;
+    p_new = new_string;
+    while (*p_old) { /* bis \0 */
+        *p_new = *p_old;
+
+        if (*p_old != ';') {
+            p_old++; p_new++;
+        } else {
+            if (PHASE_PRE_SIGN == phase) {
+                p_new++;
+                *p_new = '\r';
+            }
+            p_new++;
+            *p_new = '\n';
+            p_new++;
+            *p_new = '\t';
+            p_new++;
+            p_old++; /* Zeichen nach ; */
+            if (*p_old != ' ')
+                logmsg(LOG_ERR, "break_after_semicolon: no SPACE after ; in ->%s<-", string);
+            p_old++; /* hoffentlich das <SPACE> */
+        }
+    }
+    *p_new = '\0';
+    logmsg(LOG_DEBUG, "break_after_semicolon: replaced ->%s<- with ->%s<-", string, new_string);
+    free(string);
+    return (new_string);
+}
